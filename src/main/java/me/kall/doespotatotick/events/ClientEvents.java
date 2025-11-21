52d1f3bee092b7420a2f5d9cf6c2897763739e85
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-11-21 21:59:57 +0800

    Update ClientEvents.java

--- src/main/java/me/kall/doespotatotick/events/ClientEvents.java
+++ src/main/java/me/kall/doespotatotick/events/ClientEvents.java
@@ -2,20 +2,21 @@ package me.kall.doespotatotick.events;
 
 import me.kall.doespotatotick.config.ConfigConstants;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.item.ProjectileWeaponItem;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class ClientEvents {
     public static void bowStart(LivingEntityUseItemEvent.@NotNull Start event) {
         if (!ConfigConstants.bowSkip) return;
-        if (event.getEntity() instanceof LocalPlayer && ConfigConstants.skipRenderingUntickable) {
+        if (event.getEntity() instanceof LocalPlayer && ConfigConstants.skipRenderingUntickable && event.getItem().getItem() instanceof ProjectileWeaponItem) {
             ConfigConstants.skipRenderingUntickable = false;
         }
     }
 
     public static void bowEnd(@NotNull LivingEntityUseItemEvent event) {
         if (!ConfigConstants.bowSkip) return;
-        if (event.getEntity() instanceof LocalPlayer) {
+        if (event.getEntity() instanceof LocalPlayer && event.getItem().getItem() instanceof ProjectileWeaponItem) {
             ConfigConstants.validateClient();
         }
     }
