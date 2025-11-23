cad55f60bfefe710e1126ac365d997779307ac0d
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-11-23 21:42:01 +0800

    close https://github.com/MCTeamPotato/Kasualix-Issue-Tracker/issues/203

--- src/main/java/me/kall/doespotatotick/mixin/impl/ext/EntityMixin.java
+++ src/main/java/me/kall/doespotatotick/mixin/impl/ext/EntityMixin.java
@@ -35,6 +35,7 @@ public abstract class EntityMixin implements Tickable {
 
         this.dpt$tickable = tickable;
         Entity entity = (Entity) (Object) this;
+        if (entity.level().isClientSide()) return;
         DoesPotatoTick.CHANNEL.send(PacketDistributor.TRACKING_ENTITY.with(() -> entity), new TickablePacket(tickable, entity.getId()));
     }
 
