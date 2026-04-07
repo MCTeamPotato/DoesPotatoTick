b3d348dc39022eef50f2e40ea53d5ef7ea16956f
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2026-04-07 11:07:58 +0800

    5.6.3

--- src/main/java/me/kall/doespotatotick/integration/sodium/SodiumIntegration.java
+++ src/main/java/me/kall/doespotatotick/integration/sodium/SodiumIntegration.java
@@ -9,6 +9,7 @@ import me.jellysquid.mods.sodium.client.gui.options.OptionPage;
 import me.jellysquid.mods.sodium.client.gui.options.control.TickBoxControl;
 import me.jellysquid.mods.sodium.client.gui.options.storage.SodiumOptionsStorage;
 import me.kall.doespotatotick.DoesPotatoTick;
+import me.kall.doespotatotick.config.ConfigConstants;
 import me.kall.doespotatotick.config.TickConfig;
 import net.minecraft.network.chat.Component;
 import net.minecraftforge.common.ForgeConfigSpec;
@@ -36,7 +37,10 @@ public class SodiumIntegration {
                 .setName(Component.translatable(name))
                 .setTooltip(Component.translatable(tooltip))
                 .setControl(TickBoxControl::new)
-                .setBinding((sodiumGameOptions, value) -> config.set(value), sodiumGameOptions -> config.get())
+                .setBinding((sodiumGameOptions, value) -> {
+                    config.set(value);
+                    ConfigConstants.validateClient();
+                }, sodiumGameOptions -> config.get())
                 .setImpact(impact)
                 .build();
     }
