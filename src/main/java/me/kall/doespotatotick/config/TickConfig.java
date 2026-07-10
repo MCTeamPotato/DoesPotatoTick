93bce5d1e2f87bf619c381c401a8f04315619a88
狂想Kall <110756250+Kasualix@users.noreply.github.com>
2026-07-10 18:51:25 +0800

    resolve https://github.com/MCTeamPotato/DoesPotatoTick/issues/8

--- src/main/java/me/kall/doespotatotick/config/TickConfig.java
+++ src/main/java/me/kall/doespotatotick/config/TickConfig.java
@@ -20,7 +20,7 @@ public class TickConfig {
     static final ModConfigSpec.ConfigValue<List<? extends String>> MOB_FARM_TYPES;
 
     static {
-        List<? extends String> entityModIDList = Lists.newArrayList("create", "witherstormmod", "traveloptics", "irons_spellbooks" , "valkyrienskies", "vs_eureka", "dyairdrop");
+        List<? extends String> entityModIDList = Lists.newArrayList("create", "witherstormmod", "traveloptics", "irons_spellbooks" , "valkyrienskies", "vs_eureka", "dyairdrop", "aeronautics", "simulated", "offroad", "sable");
         List<? extends String> entityList = Lists.newArrayList("minecraft:ender_dragon", "minecraft:ghast", "minecraft:wither", "minecraft:player",
                 "alexsmobs:void_worm", "alexsmobs:void_worm_part", "alexsmobs:spectre",
                 "twilightforest:naga", "twilightforest:lich", "twilightforest:yeti", "twilightforest:snow_queen", "twilightforest:minoshroom", "twilightforest:hydra", "twilightforest:knight_phantom", "twilightforest:ur_ghast",
